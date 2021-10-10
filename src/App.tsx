e0b78b49670fe5ee12cf369aaf4f035a6508cdde
nicholasboldt <nicholas.boldt@gmail.com>
2021-10-10 14:47:33 -0700

    now can extract params ffrom the url

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import { getAuthURL } from './auth/spotifyAuth';
-import axios from 'axios'
+
+
 
 
 import './App.css';
 
 function App() {
   const authURL = getAuthURL();
+  const responseURL = window.location.href
+  
+
+  const access_token = searchCode('access_token', responseURL)
+  const token_type = searchCode('token_type', responseURL)
+  const expires_in = searchCode('expires_in', responseURL)
+  const state = searchCode('state', responseURL)
+
+  console.log(access_token)
+  console.log(token_type)
+  console.log(expires_in)
+  console.log(state)
 
   return (
     <div className="App">
-     <div><a href={authURL}>Authorise Spotify</a></div>
+      <a href={authURL}>Authorise Spotify</a>
+      {/* <p>{code ? code  : 'No code'}</p> */}
     </div>
   );
 }
+
+export const searchCode = (name : string, url = '') => {
+  const results = new RegExp("[#&]" + name + "=([^&#]*)").exec(
+    url || window.location.href
+  );
+
+  if (results == null) {
+    return null;
+  } else {
+    return results[1];
+  }
+};
 
 export default App;
