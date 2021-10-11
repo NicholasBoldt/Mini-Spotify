ff2252225d91ec2e848566dcfc277f16bf488ca7
nicholasboldt <nicholas.boldt@gmail.com>
2021-10-11 15:03:50 -0700

    getting started with reux

---
+++ src/store/playlist-slice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+createSlice({ 
+    name: 'playlists'
+    initialState: { 
+        playlists: [],
+        tracks: [],
+    },
+    reducers: {
+
+    }
+})
