d556d0859aad5487e964b808845cccb3adaae3f7
nicholasboldt <nicholas.boldt@gmail.com>
2021-10-10 17:13:57 -0700

    able to pull playlists from get request

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { getAuthURL } from './auth/spotifyAuth';
-
-
-
-
-
-import './App.css';
 import Dash from './components/Dash';
+import axios from 'axios';
 
 function App() {
+  const [playlists, setPlaylists] = useState([] as any[]);
+
+
   const authURL = getAuthURL();
   const responseURL = window.location.href
-  
+
 
   const access_token = searchCode('access_token', responseURL)
   const token_type = searchCode('token_type', responseURL)
   const expires_in = searchCode('expires_in', responseURL)
   const state = searchCode('state', responseURL)
+
+  console.log(access_token)
 
   const token = {
     access_token,
@@ -25,20 +25,36 @@ function App() {
     state,
   }
 
-  console.log(token.access_token)
-  console.log(token.token_type)
-  console.log(token.expires_in)
-  console.log(token.state)
+  const getPlaylists = async () => {
+    const response = await axios.get('https://api.spotify.com/v1/me/playlists', {
+    url: 'https://api.spotify.com/v1/me',
+    headers: {
+      'Authorization': 'Bearer ' + access_token
+    }})
+    const data: any = response.data;
+    console.log(data.items);
+  }
+
+  getPlaylists();
+  
+
+  
+
+
+
+
+  // const playlists : Promise < any[] > = d;
+
 
   return (
     <div className="App">
-      {access_token &&  <Dash />}
+      {access_token && <Dash />}
       {!access_token && <a href={authURL}>Authorise Spotify</a>}
     </div>
   );
 }
 
-const searchCode = (name : string, url = '') => {
+const searchCode = (name: string, url = '') => {
   const results = new RegExp("[#&]" + name + "=([^&#]*)").exec(
     url || window.location.href
   );
