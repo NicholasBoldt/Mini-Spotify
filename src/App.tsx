c28e5096dbce80ce0591a9ccdc9a88bab3a25eed
nicholasboldt <nicholas.boldt@gmail.com>
2021-10-11 13:49:17 -0700

    added some styling to auth button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { getAuthURL } from './auth/spotifyAuth';
 import Dash from './components/Dash';
 import { getPlaylists, getTracks } from './utiles/spotifyAPI';
+import classes from './App.module.css';
 
 function App() {
   const [playlists, setPlaylists] = useState([] as any[]);
@@ -47,9 +48,9 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className={classes.app}>
       {access_token && <Dash />}
-      {!access_token && <a href={authURL}>Authorise Spotify</a>}
+      {!access_token && <a className={classes.authorize} href={authURL}>Authorise Spotify</a>}
       <div>{tracks && tracks.map((track) => (
          <li key={track.track.id}>{track.track.name}</li>
      
